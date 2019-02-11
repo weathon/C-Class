2f41edd1fc4e1754dd1e65343bd5c24480eabce7
tcs06358 <tcs06358@163.com>
2019-02-11 16:08:13 -0500

    -

---
+++ Dual Palindromes.cpp
@@ -0,0 +1,17 @@
+//https://train.usaco.org/usacoprob2?a=qsufYybVV7s&S=dualpal
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int N,S;
+int list[16];
+int main()
+{
+	cin>>N>>S;
+	now=S;
+	for(int i=0;i<N;i++)
+	
+		//
+		now++;
+	}
+	return 0;
+}
---
+++ Transformations.cpp
@@ -0,0 +1,13 @@
+//https://train.usaco.org/usacoprob2?a=qsufYybVV7s&S=transform
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int before
+int main()
+{
+	int N;
+	cin>>N;
+	
+	
+	return 0;
+}
