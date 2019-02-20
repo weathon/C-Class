a8c699714b40f5cd1df4a8aa26923c02a1ed4f02
Wayne <tcs06358@163.com>
2019-02-20 15:04:22 -0600

    Create table.cpp

---
+++ table.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string pizza={"cheese","Sausage", "beef", "chicken"}
+    string drink={"Cola","Cocoa","Coffee", "sprite", "orange juice", "cherry juice", "watermelon juice"}
+    cout<<"We have these package:"<<endl;
+    for(int i=0;i<3i++)
+    {
+    for(int j=0;j<6;j++){
+        cout<<pizza[i]<<" with "<<drink[j];
+    }
+    
+    }
+    
+}
